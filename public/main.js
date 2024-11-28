3c0531514394c51c45687e88c216e0d28b92671a
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-11-28 07:30:10 -0800

    set up server to serve files in public directory, added comments

--- public/main.js
+++ public/main.js
@@ -1,16 +1,18 @@
+// 
 document.querySelector('button').addEventListener('click', apiRequest)
-console.log("Im connected")
+    console.log("Im connected")
 
 async function apiRequest(event){
-    console.log(`Button was pressed`)
+        console.log(`Button was pressed`)
+    // prevent refresh
     event.preventDefault();
-    console.log(`Button was pressed`)
+        console.log(`Button was pressed`)
     const rapperName = document.querySelector('input').value.toLowerCase()
-    console.log(`Rapper name is ${rapperName}`)
+        console.log(`Rapper name is ${rapperName}`)
     try{
-        // const response = await fetch(`localhost:3000/api/${rapperName}`)
+        const response = await fetch(`http://localhost:3000/api/${rapperName}`)
 
-        const response = await fetch(`https://rap-names-api-dd4y.onrender.com/api/${rapperName}`)
+        // const response = await fetch(`https://rap-names-api-dd4y.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// import and use express
 const express = require('express')
 const app = express()
+
+// import and use CORS (cross-origin resource sharing) so that the API can handle requests from other origins (like Heroku or Render)
 const cors = require('cors')
+app.use(cors())
+
+
+// specify the local port for the server to listen on
 const PORT = 3000;
 
-app.use(cors())
-app.use(express.static(__dirname));
+// tell the server to automatically serve files in this directory (so that main.js will work)
+app.use(express.static('public'));
 
 
-
+// data to be served
 const rappers = {
     '21 savage': {
         'age': 29,
@@ -26,11 +33,18 @@ const rappers = {
     }
 }
 
+// at the root endpoint, serve index.html from our current directory
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + "/index.html")
+})
 
+// at the /api endpoint, send all rapper data
 app.get('/api', (req,res) => {
     res.json(rappers)
 })
 
+// at the endoint '/api/[any given rapper name],
+// look for that name in our data, and if it exists, and serve the rapper details as a json object
 app.get('/api/:rapperName', (req,res) => {
     const rapperName = req.params.rapperName.toLowerCase()
     if(rappers[rapperName]) {
@@ -41,10 +55,7 @@ app.get('/api/:rapperName', (req,res) => {
 })
 
 
+// set up the server to listen at the port specified above, or the port in our .env file
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/index.html")
 })
